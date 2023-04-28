5dcc6118782c5de25650191ae420f8cad9879f95
Roy Mbaya <roymbaya08@gmail.com>
2023-04-29 00:26:44 +0300

    done with assignment

---
+++ src/components/GifSearch.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+function GifSearch({ onSubmit }){
+  const [query, setQuery] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    onSubmit(query);
+  }
+return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={query}
+        onChange={(event) => setQuery(event.target.value)}
+        placeholder="Search for gifs"
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+};
+
+export default GifSearch;
